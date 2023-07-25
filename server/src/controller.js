bc6bfd19642fcb34360f331f1f04d7a3ee253760
Rafid Elahi <rafidelahi.dev@gmail.com>
2023-07-25 16:40:09 +0600

    End points
    The Neccessary end points are done. Now they are good for further progress

--- server/src/controller.js
+++ server/src/controller.js
@@ -3,9 +3,54 @@ const controller = {
     console.log('hi')
     return res.send('Welcome to the home page')
   },
-  userId: (req, res) => {
-    console.log('/user')
-    return res.send('What do you wish to buy today')
+  register: (req, res) => {
+    const register_json = req.body;
+    console.log(register_json.username, register_json.password.length);
+    //Making a mock test for username taken or not
+    if (register_json.username === true && register_json.password.length >= 8) {
+      res.send('This name is already taken try something different')
+    } else {
+      res.send('Congratulations! You are our newest member on Teebay. Welcome to the community')
+    }
+},
+  logIn: (req, res) => {
+    const login_json = req.body;
+    console.log(login_json);
+    res.send("We have recieved your login data")
+  },
+  profile: (req, res) => {
+    res.send(`Your profile`)
+  },
+  myproducts: (req, res) => {
+    res.send(`Products that you have uploaded so far`)
+  },
+  myproductupdate: (req, res) => {
+    const update_json = req.body;
+    console.log(update_json);
+    res.send(`Your product ........ info was updated`)
+  },
+  myproductdelete: (req, res) => {
+    const delete_json = req.body
+    console.log(delete_json)
+    res.send(`The product ........ was deleted`)
+  },
+  myproductadd: (req, res) => {
+    const add_json = req.body;
+    console.log(add_json);
+    res.send(`New product ........ has been uploaded`)
+  },
+  allproducts: (req, res) => {
+
+    res.send(`Showing all the products available to buy or rent`)
+  },
+  allproductbuy: (req, res) => {
+    res.send(`Your purchase was successful`)
+  },
+  allproductrent: (req, res) => {
+    res.send(`You rent was successful`)
+  },
+  mystats: (req, res) => {
+    res.send(`Your stats for the things you bought, borrowed, sold, rented`)
   },
 }
 
--- server/src/index.js
+++ server/src/index.js
@@ -9,8 +9,29 @@ app.use(express.urlencoded({extended:false}))
 
 app.get('/', controller.verify)
 
-app.get('/login', controller.userId )
-app.get('/register', )
+app.post('/login', controller.logIn )
+app.post('/register', controller.register )
+
+app.get('/name', controller.profile)
+
+app.get('/name/myproducts', controller.myproducts)
+app.put('/name/myproducts/update', controller.myproductupdate)
+app.delete('/name/myproducts/delete', controller.myproductdelete)
+app.post('/name/myproducts/add', controller.myproductadd)
+
+app.get('/name/allproducts', controller.allproducts)
+app.post('/name/allproducts/buy', controller.allproductbuy)
+app.post('/name/allproducts/rent', controller.allproductrent)
+
+app.get('/name/mystats', controller.mystats)
+
+app.post('/api/data', (req, res) => {
+  const jsonData = req.body
+  console.log(jsonData)
+  res.send(`Data received successfully`)
+})
+
+
 
 app.listen(1337, () => {
   console.log('Server running just fine')
