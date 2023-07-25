e3abaff7d2fb2419b48fc5b9bc32f9f38ccf9b1d
Rafid Elahi <rafidelahi.dev@gmail.com>
2023-07-25 22:39:20 +0600

    Form Update
    The server side response to log in and registration has been updated. They are able to receive data from client and also send back data

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,56 +1,87 @@
 const controller = {
+//Home page route
   verify: (req, res) => {
     console.log('hi')
     return res.send('Welcome to the home page')
   },
+
+//register route
   register: (req, res) => {
     const register_json = req.body;
-    console.log(register_json.username, register_json.password.length);
-    //Making a mock test for username taken or not
-    if (register_json.username === true && register_json.password.length >= 8) {
+    console.log(register_json.userName, register_json.email, register_json.password);
+  //Making a mock test for username taken or not
+    // console.log(register_json.password.length >= 8)
+    // console.log(register_json.userName == true)
+    if (register_json.email == `rafidelahi.dev@gmail.com` ) {
       res.send('This name is already taken try something different')
-    } else {
-      res.send('Congratulations! You are our newest member on Teebay. Welcome to the community')
+    } else if (register_json.password.length < 8) {
+      res.send(`Password length is too short`)
     }
+    else res.send(
+      `Congratulations! You are our newest member on Teebay. Welcome to the community`
+    )
 },
+
+//logIn route
   logIn: (req, res) => {
     const {email, password} = req.body;
     console.log(email ,password);
-    res.send({
-      email, password
-    })
-  },
+  //Assuming rafid11elahi@gmail.com and password is 12345678
+    if(email == `rafid11elahi@gmail.com` ){
+      password == `12345678`? res.send('You can enter Your profile') : res.send('Your password was incorrect')
+    }
+    else {
+      res.send(`The password is incorrect. If you are new to Teebay. Please Register First`)
+  }},
+
+//my profile route
   profile: (req, res) => {
     res.send(`Your profile`)
   },
+
+//my products route
   myproducts: (req, res) => {
     res.send(`Products that you have uploaded so far`)
   },
+
+//i want to update my products route
   myproductupdate: (req, res) => {
     const update_json = req.body;
     console.log(update_json);
     res.send(`Your product ........ info was updated`)
   },
+
+//i want to delete my products route
   myproductdelete: (req, res) => {
     const delete_json = req.body
     console.log(delete_json)
     res.send(`The product ........ was deleted`)
   },
+
+//i want to add new products route
   myproductadd: (req, res) => {
     const add_json = req.body;
     console.log(add_json);
     res.send(`New product ........ has been uploaded`)
   },
+
+//i want to see all the other products route
   allproducts: (req, res) => {
 
     res.send(`Showing all the products available to buy or rent`)
   },
+
+//i want to buy a products route
   allproductbuy: (req, res) => {
     res.send(`Your purchase was successful`)
   },
+
+//i want to rent a products route
   allproductrent: (req, res) => {
     res.send(`You rent was successful`)
   },
+
+//i want to see my stats route
   mystats: (req, res) => {
     res.send(`Your stats for the things you bought, borrowed, sold, rented`)
   },
